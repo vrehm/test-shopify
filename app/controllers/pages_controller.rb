c044ffe8909c366f3b4de0c1c9979aa865a45358
Vincent REHM <vincentrehm@yahoo.fr>
2016-08-20 14:54:24 +0200

    working index whit shopify products

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @shop_url = "https://#{ENV["SHOPIFY_API_KEY"]}:#{ENV["SHOPIFY_API_PASSWORD"]}@#{ENV["SHOP_NAME"]}.myshopify.com/admin"
+    ShopifyAPI::Base.site = @shop_url
+    shop = ShopifyAPI::Shop.current
+
+    # Get a specific product
+    @products = ShopifyAPI::Product.all
   end
 end
