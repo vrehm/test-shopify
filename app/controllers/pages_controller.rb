65ff82c4550cc97c133d25a81c7f6cafca509ec0
Vincent REHM <vincentrehm@yahoo.fr>
2016-08-20 15:08:27 +0200

    remove unnecessary @var

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @shop_url = "https://#{ENV["SHOPIFY_API_KEY"]}:#{ENV["SHOPIFY_API_PASSWORD"]}@#{ENV["SHOP_NAME"]}.myshopify.com/admin"
-    ShopifyAPI::Base.site = @shop_url
+    shop_url = "https://#{ENV["SHOPIFY_API_KEY"]}:#{ENV["SHOPIFY_API_PASSWORD"]}@#{ENV["SHOP_NAME"]}.myshopify.com/admin"
+    ShopifyAPI::Base.site = shop_url
     shop = ShopifyAPI::Shop.current
 
     # Get a specific product
